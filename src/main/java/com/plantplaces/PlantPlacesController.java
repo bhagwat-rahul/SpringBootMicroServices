fc24aea56b841547731cda2def260017b62fe020
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-27 11:23:22 -0400

    Added loyalty blue, silver.

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,7 +1,9 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller 
 public class PlantPlacesController {
@@ -10,8 +12,27 @@ public class PlantPlacesController {
 	 * Handle the /start endpoint.
 	 * @return
 	 */
-	@RequestMapping("/")
-	public String start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=s"})
+	public String readSilver() {
+		return "start";
+	}
+	@RequestMapping(value="/start", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON() {
+		return "start";
+	}
+	
+	@PostMapping("/start")
+	public String create() {
 		return "start";
 	}
 	
